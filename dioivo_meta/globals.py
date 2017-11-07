4ff08a2f60ecc776189991862ecfd993fc2319e2
Diego Blanco <diego.blanco@treitos.com>
2017-11-07 22:12:15 +0100

    Version bump

--- dioivo_meta/globals.py
+++ dioivo_meta/globals.py
@@ -1,7 +1,7 @@
 # -*- coding:utf8 -*-
 # vim: set ts=2 sw=2 sts=2 et:
 
-VERSION     = "1.1.0"
+VERSION     = "1.1.1"
 AUTHOR      = "Diego Blanco"
 AUTHOR_EMAIL= "diego.blanco@treitos.com"
 URL         = "https://github.com/diego-treitos/dioivo"
